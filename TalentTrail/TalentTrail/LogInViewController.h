3230f226d410aec422e5b1bc8607f53887d189c3
pqseags <pseagren@usc.edu>
2014-04-11 23:59:40 -0700

    created loginpage

---
+++ TalentTrail/TalentTrail/LogInViewController.h
@@ -0,0 +1,17 @@
+//
+//  LogInViewController.h
+//  TalentTrail
+//
+//  Created by Parker Seagren on 4/11/14.
+//  Copyright (c) 2014 TalentTrail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+
+
+@end
---
+++ TalentTrail/TalentTrail/LogInViewController.m
@@ -0,0 +1,44 @@
+//
+//  LogInViewController.m
+//  TalentTrail
+//
+//  Created by Parker Seagren on 4/11/14.
+//  Copyright (c) 2014 TalentTrail. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@end
+
+@implementation LogInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+- (IBAction)clickedSignInButton:(id)sender {
+    
+}
+- (IBAction)clickedNewAccountButton:(id)sender {
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
