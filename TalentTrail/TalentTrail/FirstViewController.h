993bf2cf26960209ec2be3838a4d9cb5ccb19f65
pqseags <pseagren@usc.edu>
2014-04-11 23:35:49 -0700

    first commit. adding template project.

---
+++ TalentTrail/TalentTrail/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  TalentTrail
+//
+//  Created by Parker Seagren on 4/11/14.
+//  Copyright (c) 2014 TalentTrail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
