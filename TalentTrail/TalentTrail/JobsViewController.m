5393ed47c5b8d15f23058fe257128e0bb6d998ad
pqseags <pseagren@usc.edu>
2014-04-12 17:10:08 -0700

    fixed gaps

--- TalentTrail/TalentTrail/JobsViewController.m
+++ TalentTrail/TalentTrail/JobsViewController.m
@@ -34,6 +34,8 @@ NSArray *Pictures;
     
     //--------------------SIDE BAR----------------------
     // Set the side bar button action. When it's tapped, it'll show up the sidebar.
+    _sidebarButton.tintColor = [UIColor whiteColor];
+    _searchButton.tintColor = [UIColor whiteColor];
     _sidebarButton.target = self.revealViewController;
     _sidebarButton.action = @selector(revealToggle:);
     // Set the gesture
